8585fa408e3ff1315df1423e475d76ebd6333f82
HyperionNKJ <kjneo1996@gmail.com>
2020-09-27 17:38:05 +0800

    Add CORS response header

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
 let apiRoutes = require("./api-routes")
 
+
 let app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 // Connect to Mongoose and set connection variable
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/ModTrack', { useNewUrlParser: true});
