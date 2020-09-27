cf85a2481c5ad0344ac40c404a3ce3e40227b04a
HyperionNKJ <kjneo1996@gmail.com>
2020-09-27 22:31:37 +0800

    Add CORS origin and methods

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ let app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "GET, POST, OPTIONS, PUT, DELETE");
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
